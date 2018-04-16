42922669f08324cf464aaf62f0445800eca77c08
kautuk-desai <kautuk.desai@somaiya.edu>
2018-04-16 17:06:20 -0400

    added test case to find join-hive button

--- config.js
+++ config.js
@@ -1,1 +1,26 @@
 var config = {};
+
+config.email = "test@test.com";
+config.password = "password123";
+config.first_name = "test";
+config.last_name = "test";
+config.phone = 9999999999;
+config.coworker = "coworker1@test.com";
+
+// id = "join-hive"
+// $("button.play.rounded-box.state-paused").click()
+// $(".card-buttons > button.js-ob-video-continue").click()
+// $("#firstName").val("v")
+// $("#lastName").val("v")
+// $("#phone").val(9999999999)
+// $("#email")
+// $("#password")
+
+// $("input.js-workspace-input.input__field")
+// $(".organization-sizes-container > span:contains('1-10')")[0].click()
+// $(".ob-card.active .js-complete-step").click()
+// $($("#onboardingCardTeammatesForm .js-email-input")[0]).click()
+// $($("#onboardingCardTeammatesForm .js-email-input")[0]).val("coworker1@b.com").keyup()
+// $("[form='onboardingCardTeammatesForm']").click()
+// $("#onboardingCardFiles button:contains('No thanks,')").click()
+// wait until #welcomeModal is found
