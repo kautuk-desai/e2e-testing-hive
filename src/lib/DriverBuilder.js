a90243fbf0bbd1e37f4487bb1685dd5a83fe60cc
kautuk-desai <kautuk.desai@somaiya.edu>
2018-04-18 13:19:57 -0400

    modified the driver builder so that chrome driver need not be added to path variable.

--- src/lib/DriverBuilder.js
+++ src/lib/DriverBuilder.js
@@ -1,15 +1,19 @@
 var webdriver = require("selenium-webdriver")
 
 function DriverBuilder() {
+    // this will not require the chrome driver be added to path variable.
+    let chrome = require('selenium-webdriver/chrome');
+    let chrome_driver_path = require('chromedriver').path;
+    let service = new chrome.ServiceBuilder(this.chrome_driver_path).build();
+    chrome.setDefaultService(this.service);
+
     this.chrome_driver = new webdriver.Builder()
-        .forBrowser('chrome')
-        .setChromeOptions( /* … */ )
-        .build()
+        .withCapabilities(webdriver.Capabilities.chrome())
+        .build();
 }
 
-DriverBuilder.prototype.quit = function() {
+DriverBuilder.prototype.quit = function () {
     return this.chrome_driver.quit()
 }
-
 
 module.exports = DriverBuilder;
